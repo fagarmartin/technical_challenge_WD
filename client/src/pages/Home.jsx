7b72168f7d1f9e2dcdfc0e49f3604b9ba03835b3
Fatima Garcia <fagarmartin@gmail.com>
2023-06-23 13:13:22 +0200

    removed console logs

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -25,27 +25,23 @@ function Home() {
     try {
       setIsLoading(true);
       const response = await getPhoneDetails(selectedPhone);
-      console.log(response.data)
+
       setFoundPhone(response.data);
       setIsLoading(false);
-
     } catch (error) {
       navigate(error);
     }
   };
   const handleSelectedPhone = (id) => {
-
     setSelectedPhone(id);
-   
   };
 
   useEffect(() => {
     getData();
   }, []);
-  useEffect(()=>{
+  useEffect(() => {
     getDataDetails();
-  },[selectedPhone])
-  
+  }, [selectedPhone]);
 
   if (isLoading) {
     return (
@@ -57,60 +53,60 @@ function Home() {
 
   return (
     <div>
-    <div className="center-container">
-      <div className="phones-container">
-        {allPhones.map((eachPhone) => {
-          return (
-            <div className="card">
-              <Link to={"#phone-details"}
-                key={eachPhone.id}
-                onClick={() => {
-                  handleSelectedPhone(eachPhone.id);
-                }}
-              >
-                <li>
-                  <h3>{eachPhone.name}</h3>
-                  <h5>{eachPhone.manufacturer}</h5>
-                  <img
-                    src={`./images/${eachPhone.imageFileName}`}
-                    alt={eachPhone.name}
-                  />
-                </li>
-              </Link>
-            </div>
-          );
-        })}
-      </div>
-      
-    </div>
-    {foundPhone && (
-      <div className="phone-details">
-        <h1>{foundPhone.name}</h1>
-        <h2>{foundPhone.manufacturer}</h2>
-        <div className="center-container details">
-          <div className="img-phone-details">
-            <img
-              src={`../images/${foundPhone.imageFileName}`}
-              alt={foundPhone.name}
-            />
-          </div>
-          <div className="phone-info">
-            <h5>Color:</h5>
-            <p>{foundPhone.color}</p>
-            <h5>Description:</h5>
-            <p>{foundPhone.description}</p>
-            <h5>Price:</h5>
-            <p>{foundPhone.price}</p>
-            <h5>Processor:</h5>
-            <p>{foundPhone.processor}</p>
-            <h5>Ram:</h5>
-            <p>{foundPhone.ram}</p>
-            <h5>Screen:</h5>
-            <p>{foundPhone.screen}</p>
-          </div>
+      <div className="center-container">
+        <div className="phones-container">
+          {allPhones.map((eachPhone) => {
+            return (
+              <div className="card"  key={eachPhone.id}>
+                <Link
+                  to={"#phone-details"}
+                 
+                  onClick={() => {
+                    handleSelectedPhone(eachPhone.id);
+                  }}
+                >
+                  <li>
+                    <h3>{eachPhone.name}</h3>
+                    <h5>{eachPhone.manufacturer}</h5>
+                    <img
+                      src={`./images/${eachPhone.imageFileName}`}
+                      alt={eachPhone.name}
+                    />
+                  </li>
+                </Link>
+              </div>
+            );
+          })}
         </div>
       </div>
-    )}
+      {foundPhone && (
+        <div className="phone-details">
+          <h1>{foundPhone.name}</h1>
+          <h2>{foundPhone.manufacturer}</h2>
+          <div className="center-container details">
+            <div className="img-phone-details">
+              <img
+                src={`../images/${foundPhone.imageFileName}`}
+                alt={foundPhone.name}
+              />
+            </div>
+            <div className="phone-info">
+              <h5>Color:</h5>
+              <p>{foundPhone.color}</p>
+              <h5>Description:</h5>
+              <p>{foundPhone.description}</p>
+              <h5>Price:</h5>
+              <p>{foundPhone.price}</p>
+              <h5>Processor:</h5>
+              <p>{foundPhone.processor}</p>
+              <h5>Ram:</h5>
+              <p>{foundPhone.ram}</p>
+              <h5>Screen:</h5>
+              <p>{foundPhone.screen}</p>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
